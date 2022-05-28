347e640a0155ba2301166d4208c9046e9cbb8465
Petar Barisic <petar.barisic1999@gmail.com>
2022-05-28 15:19:02 +0200

    Initial commit

---
+++ app/src/main/java/petarbarisic/ferit/momentmap/MainActivity.kt
@@ -0,0 +1,11 @@
+package petarbarisic.ferit.momentmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
